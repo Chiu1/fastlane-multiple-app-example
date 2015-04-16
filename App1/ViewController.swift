2bc512e9f22e70d03f01f50e0d70ce1a00242cee
iamjason <jasongarrett@Jasons-MacBook-Pro.local>
2015-04-15 21:40:12 -0400

    added project (currently not passing EVN variables correctly)

---
+++ App1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App1
+//
+//  Created by Jason Garrett on 4/15/15.
+//  Copyright (c) 2015 jg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fastlane-multiple-app-example
 A barebones example of the how I'm trying to use fastlane with multiple applications within a single workspace.
+
+Run ``` build_App1.sh ``` & ``` build_App2.sh ```
